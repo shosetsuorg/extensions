d5b15c42aaf9ce83b9a867cf64ec7adcd10da4df
TechnoJo4 <technojo4@gmail.com>
2020-01-19 10:19:17 -0500

    Fix BoxNovel

--- src/main/resources/BoxNovel.lua
+++ src/main/resources/BoxNovel.lua
@@ -2,7 +2,7 @@
 --- @author Doomsdayrs
 --- @version 1.0.0
 
-luajava = require("luajava")
+local luajava = require("luajava")
 
 --local LuaSupport = luajava.newInstance("com.github.doomsdayrs.api.shosetsu.services.core.objects.LuaSupport")
 local baseURL = "https://boxnovel.com"
@@ -71,83 +71,80 @@ end
 --- @param document : Jsoup document of the page with chapter text on it
 --- @return string passage of chapter, If nothing can be parsed, then the text should be describing of why there isn't a chapter
 function getNovelPassage(document)
-    paragraphs = document:select("div.text-left"):select("p")
-    passage = ""
-    for i = 0, paragraphs:size() - 1, 1 do
-        passage = passage .. paragraphs:get(i):toString() .. "\n"
+    local paragraphs = document:select("div.text-left"):select("p")
+    local t = {}
+    for i=1, paragraphs:size(), 1 do
+        t[i] = paragraphs:get(i-1):text()
     end
-    return string.gsub(string.gsub(passage, "</p>", ""), "<p>", "")
+    return table.concat(t, "\n"):gsub("</?p>", "")
 end
 
 --- @param document : Jsoup document of the novel information page
 --- @return NovelPage : java object
 function parseNovel(document)
-    novelPage = LuaSupport:getNovelPage()
+    local novelPage = LuaSupport:getNovelPage()
     novelPage:setImageURL(document:selectFirst("div.summary_image"):selectFirst("img.img-responsive"):attr("src"))
     novelPage:setTitle(document:selectFirst("h3"):text())
     novelPage:setDescription(document:selectFirst("p"):text())
 
     -- Info
-    elements = document:selectFirst("div.post-content"):select("div.post-content_item")
-    for i = 1, elements:size() - 1, 1 do
-        subElements = nil
-        if i == 0 or i == 1 or i == 2 or i == 6 then
-        elseif i == 3 then
-            subElements = elements:get(i):select("a")
-            authors = LuaSupport:getStringArray()
-            authors:setSize(subElements:size())
-            for y = 0, subElements:size() - 1, 1 do
-                authors:setPosition(y, subElements:get(y):text())
+    do
+        local elements = document:selectFirst("div.post-content"):select("div.post-content_item")
+
+        -- authors
+        do
+            local authorE = elements:get(3):select("a")
+            local authors = LuaSupport:getStringArray()
+            authors:setSize(authorE:size())
+            for y = 0, authorE:size() - 1, 1 do
+                authors:setPosition(y, authorE:get(y):text())
             end
             novelPage:setAuthors(authors:getStrings())
-        elseif i == 4 then
-            subElements = elements:get(i):select("a")
-            artists = LuaSupport:getStringArray()
-            artists:setSize(subElements:size())
-            for y = 0, subElements:size() - 1, 1 do
-                artists:setPosition(y, subElements:get(y):text())
+        end
+        -- artists
+        do
+            local artistE = elements:get(4):select("a")
+            local artists = LuaSupport:getStringArray()
+            artists:setSize(artistE:size())
+            for y = 0, artistE:size() - 1, 1 do
+                artists:setPosition(y, artistE:get(y):text())
             end
             novelPage:setArtists(artists:getStrings())
-        elseif i == 5 then
-            subElements = elements:get(i):select("a")
-            genres = LuaSupport:getStringArray()
-            genres:setSize(subElements:size())
-            for y = 0, subElements:size() - 1, 1 do
-                genres:setPosition(y, subElements:get(y):text())
+        end
+        -- genres
+        do
+            local genreE = elements:get(5):select("a")
+            local genres = LuaSupport:getStringArray()
+            genres:setSize(genreE:size())
+            for y = 0, genreE:size() - 1, 1 do
+                genres:setPosition(y, genreE:get(y):text())
             end
             novelPage:setArtists(genres:getStrings())
         end
-    end
 
-    elements = document:selectFirst("div.post-status"):select("div.post-content_item")
-    for i = 0, elements:size() - 1, 1 do
-        if i == 0 or i == 2 then
-        elseif i == 1 then
-            status = elements:get(i):select("div.summary-content"):text()
-            if status == "OnGoing" then
-                novelPage:setStatus(LuaSupport:getStatus(0))
-            elseif status == "Completed" then
-                novelPage:setStatus(LuaSupport:getStatus(1))
-            end
-        end
+        -- sorry for this extremely long line
+        novelPage:setStatus(LuaSupport:getStatus((
+            document:selectFirst("div.post-status"):select("div.post-content_item"):get(1)
+                :select("div.summary-content"):text() == "OnGoing") and 0 or 1))
     end
 
     -- Chapters
-    novelChapters = LuaSupport:getCAL()
-    elements = document:select("li.wp-manga-chapter")
-    a = elements:size()
-    for i = 0, elements:size() - 1, 1 do
-        element = elements:get(i)
-        novelChapter = LuaSupport:getNovelChapter()
-        novelChapter:setLink(element:selectFirst("a"):attr("href"))
-        novelChapter:setTitle(element:selectFirst("a"):text())
-        novelChapter:setRelease(element:selectFirst("i"):text())
-        novelChapter:setOrder(a)
-        a = a - 1
-        novelChapters:add(novelChapter)
+    do
+        local novelChapters = LuaSupport:getCAL()
+        local elements = document:select("li.wp-manga-chapter")
+        local a = elements:size()
+        for i=0, a-1 do
+            local element = elements:get(i)
+            local novelChapter = LuaSupport:getNovelChapter()
+            novelChapter:setLink(element:selectFirst("a"):attr("href"))
+            novelChapter:setTitle(element:selectFirst("a"):text())
+            novelChapter:setRelease(element:selectFirst("i"):text())
+            novelChapter:setOrder(a - i)
+            novelChapters:add(novelChapter)
+        end
+        novelChapters = LuaSupport:reverseAL(novelChapters)
+        novelPage:setNovelChapters(novelChapters)
     end
-    novelChapters = LuaSupport:reverseAL(novelChapters)
-    novelPage:setNovelChapters(novelChapters)
 
     return novelPage
 end
@@ -156,7 +153,7 @@ end
 --- @param increment number : Page #
 --- @return NovelPage : java object
 function parseNovelI(document, increment)
-    print("LUA: Passing novel")
+    print("Lua: Passing novel")
     return parseNovel(document)
 end
 
@@ -166,43 +163,35 @@ function novelPageCombiner(url, increment)
     return url
 end
 
---- @param document : Jsoup document of latest listing
---- @return Array : Novel array list
-function parseLatest(document)
-    novels = LuaSupport:getNAL()
-    novelsHTML = document:select("div.col-xs-12.col-md-6")
+local function parseNovelList(doc, sel)
+    local novels = LuaSupport:getNAL()
+    local novelsHTML = doc:select(sel)
     for i = 0, novelsHTML:size() - 1, 1 do
-        novel = LuaSupport:getNovel()
-        data = novelsHTML:get(i):selectFirst("a")
+        local novel = LuaSupport:getNovel()
+        local data = novelsHTML:get(i):selectFirst("a")
         novel:setTitle(data:attr("title"))
         novel:setLink(data:attr("href"))
         novel:setImageURL(data:selectFirst("img"):attr("src"))
         novels:add(novel)
     end
     return novels
+end
+
+--- @param document : Jsoup document of latest listing
+--- @return Array : Novel array list
+function parseLatest(document)
+    return parseNovelList("div.col-xs-12.col-md-6")
 end
 
 --- @param document : Jsoup document of search results
 --- @return Array : Novel array list
 function parseSearch(document)
-    novels = LuaSupport:getNAL()
-    novelsHTML = document:select("div.c-tabs-item__content")
-    for i = 0, novelsHTML:size() - 1, 1 do
-        novel = LuaSupport:getNovel()
-        data = novelsHTML:get(i):selectFirst("a")
-        novel:setTitle(data:attr("title"))
-        novel:setLink(data:attr("href"))
-        novel:setImageURL(data:selectFirst("img"):attr("src"))
-        novels:add(novel)
-    end
-    return novels
+    return parseNovelList("div.c-tabs-item__content")
 end
 
 --- @param query string query to use
 --- @return string url
 function getSearchString(query)
-    query = string.gsub(query, "\+", "%2")
-    query = string.gsub(query, " ", "+")
-    return baseURL .. "/?s=" .. query .. "&post_type=wp-manga"
+    return baseURL .. "/?s=" .. query:gsub(query, "%+", "%2"):gsub(query, " ", "+") .. "&post_type=wp-manga"
 end
 
