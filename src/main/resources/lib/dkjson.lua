19e7b9a493f52fa809f6af425aa516eaf0b1b09c
TechnoJo4 <technojo4@gmail.com>
2020-02-08 12:12:27 -0500

    fix warnings & useless var

--- src/main/resources/lib/dkjson.lua
+++ src/main/resources/lib/dkjson.lua
@@ -42,8 +42,8 @@ SOFTWARE.
 --]==]
 
 -- global dependencies:
-local pairs, type, tostring, tonumber, getmetatable, setmetatable, rawset =
-pairs, type, tostring, tonumber, getmetatable, setmetatable, rawset
+local pairs, type, tostring, tonumber, getmetatable, setmetatable =
+pairs, type, tostring, tonumber, getmetatable, setmetatable
 local error, require, pcall, select = error, require, pcall, select
 local floor, huge = math.floor, math.huge
 local strrep, gsub, strsub, strbyte, strchar, strfind, strlen, strformat =
@@ -58,7 +58,6 @@ if register_global_module_table then
     _G[global_module_name] = json
 end
 
-local _ENV = nil -- blocking globals in Lua 5.2
 
 pcall (function()
     -- Enable access to blocked metatables.
@@ -504,7 +503,6 @@ end
 local scanvalue -- forward declaration
 
 local function scantable (what, closechar, str, startpos, nullval, objectmeta, arraymeta)
-    local len = strlen (str)
     local tbl, n = {}, 0
     local pos = startpos + 1
     if what == 'object' then
--- src/main/resources/src/en/ReadNovelFull.lua
+++ src/main/resources/src/en/ReadNovelFull.lua
@@ -1,8 +1,6 @@
 -- {"id":278,"version":"1.0.0","author":"TechnoJo4","repo":""}
 
-local lib = Require("NovelFull")
-
-return lib("https://readnovelfull.com", {
+return Require("NovelFull")("https://readnovelfull.com", {
     id = 278,
     name = "ReadNovelFull",
     imageURL = "",
