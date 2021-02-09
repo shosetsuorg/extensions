233339159b756c72d84290d5929ed5aa98b674cb
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-02-09 15:00:49 +0000

    Add icons for new sources

--- src/main/resources/src/en/AsianHobbyist.lua
+++ src/main/resources/src/en/AsianHobbyist.lua
@@ -1,4 +1,4 @@
--- {"id":951,"ver":"1.0.0","libVer":"1.0.0","author":"Doomsdayrs"}
+-- {"id":951,"ver":"1.0.1","libVer":"1.0.0","author":"Doomsdayrs"}
 
 local baseURL = "https://www.asianhobbyist.com"
 
@@ -84,6 +84,7 @@ return {
 	id = 951,
 	name = "Asian Hobbyist",
 	baseURL = baseURL,
+	imageURL = "https://github.com/shosetsuorg/extensions/raw/dev/src/main/resources/icons/AsianHobbyist.png",
 	hasSearch = true,
 	listings = {
 		Listing("Latest", false, function()
