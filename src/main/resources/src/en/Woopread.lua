3d88a73196b17d173b698934d9c076c02344f60e
Doomsdayrs <38189170+Doomsdayrs@users.noreply.github.com>
2021-02-08 20:28:11 -0500

    Woopread fix (#101)

--- src/main/resources/src/en/Woopread.lua
+++ src/main/resources/src/en/Woopread.lua
@@ -1,4 +1,4 @@
--- {"id":4217,"ver":"1.0.1","libVer":"1.0.0","author":"TechnoJo4","dep":["Madara>=1.2.2"]}
+-- {"id":4217,"ver":"1.0.2","libVer":"1.0.0","author":"TechnoJo4","dep":["Madara>=1.2.2"]}
 
 return Require("Madara")("https://woopread.com", {
     id = 4217,
@@ -44,4 +44,5 @@ return Require("Madara")("https://woopread.com", {
         ["complete"] = "Completed",
         ["slice-of-life"] = "Slice of Life",
     },
+    doubleLoadChapters = true
 })
