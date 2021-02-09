5e5c1968717730c0b29c62ffaf774518b67d8751
doomsdayrs <doomsdayrs@gmail.com>
2021-02-08 22:37:01 -0500

    Added lightnovelheaven.com

---
+++ src/main/resources/src/en/LightNovelHeaven.lua
@@ -0,0 +1,11 @@
+-- {"id":2925,"ver":"1.0.0","libVer":"1.0.0","author":"Doomsdayrs","dep":["Madara>=1.1.0"]}
+
+return Require("Madara")("https://lightnovelheaven.com", {
+	id = 2925,
+	name = "Light Novel Heaven",
+	imageURL = "https://lightnovelheaven.com/wp-content/uploads/2017/10/lnhlast.png",
+	genres = {},
+	novelListingURLPath = "novel-list",
+	shrinkURLNovel = "series",
+	doubleLoadChapters = true
+})
