2256881ff8dc63b1184defd41907a99cd0bd236b
doomsdayrs <doomsdayrs@gmail.com>
2021-02-08 20:26:06 -0500

    Reimplemented Foxaholic

--- src/main/resources/src/en/Foxaholic.lua
+++ src/main/resources/src/en/Foxaholic.lua
@@ -1,4 +1,4 @@
--- {"id":173,"ver":"1.0.1","libVer":"1.0.0","author":"TechnoJo4","dep":["Madara>=1.1.0"]}
+-- {"id":173,"ver":"1.0.2","libVer":"1.0.0","author":"TechnoJo4","dep":["Madara>=1.1.0"]}
 
 return Require("Madara")("https://foxaholic.com", {
 	id = 173,
