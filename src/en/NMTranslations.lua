4eca61147b4a6193043001ed1667d99ecea855cb
doomsdayrs <doomsdayrs@gmail.com>
2021-07-19 10:12:30 -0400

    Finish first draft of NMTranslations.lua

--- src/en/NMTranslations.lua
+++ src/en/NMTranslations.lua
@@ -2,21 +2,60 @@
 local baseURL = "https://www.nanomashin.online"
 
 --- @param novelURL string @URL of novel
+--- @param loadChapters boolean
 --- @return NovelInfo
-local function parseNovel(novelURL)
+local function parseNovel(novelURL, loadChapters)
 	local novelInfo = NovelInfo()
-	local document = GETDocument(novelURL)
+	local document = GETDocument(baseURL .. novelURL)
 	novelInfo:setTitle(document:selectFirst("h1.text-2xl"):text())
 	novelInfo:setImageURL(baseURL .. document:selectFirst("img.object-contain"):attr("src"))
 	novelInfo:setDescription(document:selectFirst("div.pt-6.pb-8"):selectFirst("div.pt-6.pb-8"):text())
 
-	local navBox = document:selectFirst("nav.flex")
+	if loadChapters then
+		local navBox = document:selectFirst("nav.flex")
+		local chapters = {}
 
-	if navBox ~= nil then
+		--- @param chaptersDocument Document
+		local function parseChapters(chaptersDocument)
+			return map(chaptersDocument:select("article.space-y-2"), function(article)
+				local c = NovelChapter()
+				local titleElement = article:selectFirst("a")
+				c:setTitle(titleElement:text())
+				c:setLink(titleElement:attr("href"))
+				c:setRelease(article:selectFirst("time"):text())
+				return c
+			end)
+		end
 
+		chapters = chapters + parseChapters(document)
+		-- There is more chapters via pages
+		if navBox ~= nil then
+			local maxPage = tonumber(navBox:selectFirst("span"):text():gsub("%d of", ""))
+
+			for page = 2, maxPage do
+				local chaptersDocument = GETDocument(baseURL .. novelURL .. "/page/" .. page)
+				chapters = chapters + parseChapters(chaptersDocument)
+			end
+		end
+
+		local chaptersList = AsList(chapters)
+		Reverse(chaptersList)
+		novelInfo:setChapters(chaptersList)
 	end
-
 end
+
+---@param v Element | Elements
+local function text(v)
+	return v:text()
+end
+
+--- @param chapterURL string @url of the chapter
+--- @return string @of chapter
+local function getPassage(chapterURL)
+	local d = GETDocument(baseURL .. "/" .. chapterURL)
+	return table.concat(map(d:selectFirst("div.pt-10"):select("p"), text), "\n")
+end
+
 
 return {
 	id = 93082,
@@ -33,7 +72,7 @@ return {
 				local title = v:selectFirst("h2.mb-3")
 
 				lis:setTitle(title:text())
-				lis:setLink(baseURL .. title:selectFirst("href"))
+				lis:setLink(title:selectFirst("href"))
 
 				local imageURL = v:selectFirst("img.object-cover")
 				lis:setImageURL(baseURL .. imageURL:attr("src"))
