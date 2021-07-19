d16ec5be9b592ad43195f303e3c6b94d80df156f
TechnoJo4 <technojo4@gmail.com>
2021-07-19 16:08:07 -0400

    Update WuxiaWorld and VolareNovels to HTML chapters

--- src/en/VolareNovels.lua
+++ src/en/VolareNovels.lua
@@ -1,3 +1,4 @@
--- {"id":1274,"ver":"1.0.2","libVer":"1.0.0","author":"TechnoJo4","dep":["WWVolare>=1.0.1"]}
+-- {"id":1274,"ver":"1.0.1","libVer":"1.0.0","author":"TechnoJo4","dep":["WWVolare>=1.0.1"]}
 
-return Require("WWVolare")(1274, "VolareNovels", "https://www.volarenovels.com", ".jfontsize_content", "https://github.com/shosetsuorg/extensions/raw/dev/icons/VolareNovels.png")
+return Require("WWVolare")(1274, "VolareNovels", "https://www.volarenovels.com", ".jfontsize_content",
+		"https://github.com/shosetsuorg/extensions/raw/dev/icons/VolareNovels.png")
