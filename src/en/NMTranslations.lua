3ce2e54cd913aae53e816faf383f9acf33686215
TechnoJo4 <technojo4@gmail.com>
2021-07-19 15:37:23 -0400

    Restructure into idiomatic Lua code

--- src/en/NMTranslations.lua
+++ src/en/NMTranslations.lua
@@ -1,59 +1,9 @@
 -- {"id":93082,"ver":"1.0.0","libVer":"1.0.0","author":"Doomsdayrs","dep":[]}
+
 local baseURL = "https://www.nanomashin.online"
 
---- @param novelURL string @URL of novel
---- @param loadChapters boolean
---- @return NovelInfo
-local function parseNovel(novelURL, loadChapters)
-	local novelInfo = NovelInfo()
-	local document = GETDocument(baseURL .. novelURL)
-	novelInfo:setTitle(document:selectFirst("h1.text-2xl"):text())
-	novelInfo:setImageURL(baseURL .. document:selectFirst("img.object-contain"):attr("src"))
-	novelInfo:setDescription(document:selectFirst("div.pt-6.pb-8"):selectFirst("div.pt-6.pb-8"):text())
-
-	if loadChapters then
-		--- @param chaptersDocument Document
-		local function parseChapters(chaptersDocument)
-			return map(chaptersDocument:select("article.space-y-2"), function(article)
-				local c = NovelChapter()
-				local titleElement = article:selectFirst("a")
-				c:setTitle(titleElement:text())
-				c:setLink(titleElement:attr("href"))
-				c:setRelease(article:selectFirst("time"):text())
-				return c
-			end)
-		end
-
-		local chapters = {}
-		local navBox = document:selectFirst("nav.flex")
-
-		chapters = chapters + parseChapters(document)
-		-- There is more chapters via pages
-		if navBox ~= nil then
-			local maxPage = tonumber(navBox:selectFirst("span"):text():gsub("%d of", ""))
-
-			for page = 2, maxPage do
-				local chaptersDocument = GETDocument(baseURL .. novelURL .. "/page/" .. page)
-				chapters = chapters + parseChapters(chaptersDocument)
-			end
-		end
-
-		local chaptersList = AsList(chapters)
-		Reverse(chaptersList)
-		novelInfo:setChapters(chaptersList)
-	end
-end
-
----@param v Element | Elements
 local function text(v)
 	return v:text()
-end
-
---- @param chapterURL string @url of the chapter
---- @return string @of chapter
-local function getPassage(chapterURL)
-	local d = GETDocument(baseURL .. "/" .. chapterURL)
-	return table.concat(map(d:selectFirst("div.pt-10"):select("p"), text), "\n")
 end
 
 return {
@@ -64,26 +14,64 @@ return {
 	hasSearch = false,
 	listings = {
 		Listing("Projects", false, function()
-			local d = GETDocument(baseURL .. "/projects")
+			local doc = GETDocument(baseURL .. "/projects")
 
-			return map(d:select("div.p-4"), function(v)
-				local lis = Novel()
+			return map(doc:select("div.p-4"), function(v)
 				local title = v:selectFirst("h2.mb-3"):selectFirst("a")
-				lis:setTitle(title:text())
-				lis:setLink(title:attr("href"))
-
-				local imageURL = v:selectFirst("img.object-cover")
-				lis:setImageURL(baseURL .. imageURL:attr("src"))
-				return lis
+				return Novel {
+					title = title:text(),
+					link = title:attr("href"),
+					imageURL = baseURL .. v:selectFirst("img.object-cover"):attr("src"),
+				}
 			end)
 		end)
 	},
-	getPassage = getPassage,
-	parseNovel = parseNovel,
-	--settings = {
-	--  DropdownFilter(1, "Language", { "English", "Chinese" })
-	--},
-	--updateSetting = function(id, value)
-	--	settings[id] = value
-	--end
+
+	parseNovel = function(url, loadChapters)
+		local document = GETDocument(baseURL .. url)
+
+		local info = NovelInfo {
+			title = document:selectFirst("h1.text-2xl"):text(),
+			imageURL = baseURL .. document:selectFirst("img.object-contain"):attr("src"),
+			description = document:selectFirst("div.pt-6.pb-8"):selectFirst("div.pt-6.pb-8"):text(),
+		}
+
+		if loadChapters then
+			--- @param chaptersDocument Document
+			local function parseChapters(chaptersDocument)
+				return map(chaptersDocument:select("article.space-y-2"), function(article)
+					local titleElement = article:selectFirst("a")
+					return NovelChapter {
+						title = titleElement:text(),
+						link = titleElement:attr("href"),
+						release = article:selectFirst("time"):text()
+					}
+				end)
+			end
+
+			local chapters = { parseChapters(document) }
+
+			-- There can be more chapters in other pages
+			local navBox = document:selectFirst("nav.flex")
+			if navBox then
+				local maxPage = tonumber(navBox:selectFirst("span"):text():match("of (%d+)"))
+
+				for page = 2, maxPage do
+					local chaptersDocument = GETDocument(baseURL .. url .. "/page/" .. page)
+					chapters[page] = parseChapters(chaptersDocument)
+				end
+			end
+
+			local chaptersList = AsList(flatten(chapters))
+			Reverse(chaptersList)
+			info:setChapters(chaptersList)
+		end
+
+		return info
+	end,
+
+	getPassage = function(url)
+		local doc = GETDocument(baseURL .. "/" .. url)
+		return table.concat(map(doc:selectFirst("div.pt-10"):select("p"), text), "\n")
+	end
 }
