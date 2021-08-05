359e0cd5d766f69c23f93588576b452b1eecac83
Dunbock <8299260+Dunbock@users.noreply.github.com>
2021-08-05 17:45:05 +0200

    Debug AsianHobbyist Image

--- src/en/AsianHobbyist.lua
+++ src/en/AsianHobbyist.lua
@@ -96,7 +96,7 @@ return {
 			return map(document:select("li.item"), function(v)
 				local a = v:selectFirst("a")
 				local image = a:selectFirst("img")
-				Log("AHNImage", image:attr("src"))
+				Log("AHNImage", image:toString())
 				return Novel {
 					title = image:attr("alt"),
 					imageURL = image:attr("src"),
