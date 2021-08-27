0402e1e4e2428fd39c3e0a4935d921a938d03b56
jpdasma <20491227+jpdasma@users.noreply.github.com>
2021-08-27 22:17:53 +0800

    Fix nanomashin.online (#165)
    Closes #164 
    
    Remove "/projects" from URL

--- src/en/NMTranslations.lua
+++ src/en/NMTranslations.lua
@@ -1,5 +1,4 @@
--- {"id":93082,"ver":"1.0.0","libVer":"1.0.0","author":"Doomsdayrs","dep":[]}
-
+-- {"id":93082,"ver":"1.0.1","libVer":"1.0.0","author":"Doomsdayrs","dep":[]}
 local baseURL = "https://www.nanomashin.online"
 
 local function text(v)
@@ -14,7 +13,7 @@ return {
 	hasSearch = false,
 	listings = {
 		Listing("Projects", false, function()
-			local doc = GETDocument(baseURL .. "/projects")
+			local doc = GETDocument(baseURL)
 
 			return map(doc:select("div.p-4"), function(v)
 				local title = v:selectFirst("h2.mb-3"):selectFirst("a")
