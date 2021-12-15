bedd4193b5a7479a4941835e53c873f147b4b845
Dunbock <8299260+Dunbock@users.noreply.github.com>
2021-12-15 15:12:07 +0100

    RLN Update Listing; Add missing Dependencies (#225)
    Adjust RLN to change from _top-novel_ to _top-novels_ in the default listing.
    Add the missing dependencies to RewayatClub and RLN.

--- src/en/ReadLightNovel.lua
+++ src/en/ReadLightNovel.lua
@@ -1,4 +1,4 @@
--- {"id":6118,"ver":"2.0.3","libVer":"1.0.0","author":"TechnoJo4"}
+-- {"id":6118,"ver":"2.0.4","libVer":"1.0.0","author":"TechnoJo4","dep":["url>=1.0.0"]}
 
 local baseURL = "https://www.readlightnovel.me"
 local qs = Require("url").querystring
@@ -51,7 +51,7 @@ return {
 
 	listings = {
 		Listing("Top Novels", true, function(data)
-			return parseTop(GETDocument(baseURL .. "/top-novel/" .. data[PAGE]))
+			return parseTop(GETDocument(baseURL .. "/top-novels/" .. data[PAGE]))
 		end)
 	},
 
