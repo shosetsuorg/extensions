cd0379c205614ea305f2728ed3e7c677bed31453
doomsdayrs <doomsdayrs@gmail.com>
2022-02-22 14:03:14 -0500

    Implement morenovel.net

---
+++ src/ind/MoreNovel.lua
@@ -0,0 +1,46 @@
+-- {"id":823465,"ver":"2.1.0","libVer":"1.0.0","author":"Doomsdayrs","dep":["Madara>=2.2.0"]}
+
+return Require("Madara")("https://morenovel.net",{
+	id = 823465,
+	name = "MORENOVEL",
+	imageURL = "https://morenovel.net/wp-content/uploads/2019/04/morenovel-1.png",
+
+	-- defaults values
+	latestNovelSel = "div.col-12.col-md-6.badge-pos-1",
+
+	genres = {
+		"Action",
+		"Adventure",
+		"Comedy",
+		"Drama",
+		"Ecchi",
+		"Fantasy",
+		"Gender Bender",
+		"Harem",
+		"Historical",
+		"Horror",
+		"Josei",
+		"Martial Arts",
+		"Mature",
+		"Mecha",
+		"Mystery",
+		"Psychological",
+		"Romance",
+		"School Life",
+		"Sci-fi",
+		"Seinen",
+		"Shoujo",
+		"Shoujo Ai",
+		"Shounen",
+		"Shounen Ai",
+		"Slice of Life",
+		"Sports",
+		"Supernatural",
+		"Tragedy",
+		"Wuxia",
+		"Xianxia",
+		"Xuanhuan",
+		"Yaoi",
+		"Yuri"
+	}
+})
