a42eacad0c9845eab3dd827943e06ee27bf065a2
doomsdayrs <doomsdayrs@gmail.com>
2022-02-22 13:35:34 -0500

    Implement Novelteam.lua

---
+++ src/en/Novelteam.lua
@@ -0,0 +1,50 @@
+-- {"id":724098,"ver":"2.1.0","libVer":"1.0.0","author":"Doomsdayrs","dep":["Madara>=2.2.0"]}
+
+return Require("Madara")("https://novelteam.net/",{
+	id = 724098,
+	name = "NOVEL TEAM",
+	imageURL = "https://novelteam.net/wp-content/themes/madara/images/logo.png",
+
+	-- defaults values
+	latestNovelSel = "div.col-12.col-md-6.badge-pos-1",
+
+	genres = {
+		"Action",
+		"Adult",
+		"Adventure",
+		"Comedy",
+		"Drama",
+		"Ecchi",
+		"Fantasy",
+		"Gender Bender",
+		"Harem",
+		"Historical",
+		"Horror",
+		"Josei",
+		"Magical Realism",
+		"Martial Arts",
+		"Mature",
+		"Mecha",
+		"Mystery",
+		"Psychological",
+		"Romance",
+		"School Life",
+		"Sci-fi",
+		"Seinen",
+		"Shoujo",
+		"Shoujo Ai",
+		"Shounen",
+		"Shounen Ai",
+		"Slice of Life",
+		"Smut",
+		"Sports",
+		"Supernatural",
+		"Tragedy",
+		"Video Games",
+		"Wuxia",
+		"Xianxia",
+		"Xuanhuan",
+		"Yaoi",
+		"Yuri"
+	}
+})
