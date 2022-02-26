4ea5ee3f9d4fb97e4ed0428e851079d4c7d0aa24
Dunbock <8299260+Dunbock@users.noreply.github.com>
2022-02-26 18:31:22 +0000

    Repair Octopii (#289)
    This is basically a re-implementation due to severe website changes.

--- src/en/Octopii.lua
+++ src/en/Octopii.lua
@@ -1,8 +1,10 @@
--- {"id":420,"ver":"1.0.0","libVer":"1.0.0","author":"Dunbock","dep":["url>=1.0.0"]}
+-- {"id":420,"ver":"2.0.0","libVer":"1.0.0","author":"Dunbock","dep":["dkjson>=1.0.1"]}
 
 local baseURL = "https://octopii.co"
 
-local encode = Require("url").encode
+local json = Require("dkjson")
+
+local MTYPE = MediaType("application/json")
  
 local function shrinkURL(url)
 	return url:gsub(".-octopii%.co", "")
@@ -15,10 +17,10 @@ end
 local text = function(v) return v:text() end
 
 local function parseTop(doc)
-	return map(doc:selectFirst("main#primary"):select("div.col-12.col-md-6"), function(v)
-		local e = v:selectFirst("h3.novel-name a")
+	return map(doc:selectFirst("main div.row.mt-4"):select("div.col-12.col-sm-6"), function(v)
+		local e = v:selectFirst("div.novel-info > a")
 		return Novel {
-			title = text(e),
+			title = e:selectFirst("h4"):text(),
 			link = shrinkURL(e:attr("href")),
 			imageURL = v:selectFirst("img"):attr("src")
 		}
@@ -37,16 +39,22 @@ return {
 
 	listings = {
 		Listing("Top Novels", true, function(data)
-			return parseTop(GETDocument(expandURL("/novel-list/page/" .. data[PAGE] .. "/?m_orderby=latest")))
+			return parseTop(GETDocument(expandURL("/latest-release?page=" .. data[PAGE])))
 		end)
 	},
 
 	getPassage = function(chapterURL)
-		local htmlElement = GETDocument(expandURL(chapterURL)):selectFirst("header.entry-header")
+		local htmlElement = GETDocument(expandURL(chapterURL)):selectFirst("div.content")
+
+        -- Change header from h5 to something bigger.
+        htmlElement:traverse(NodeVisitor(function(v)
+            if v:tagName() == "h5" then
+                v:tagName("h1")
+            end
+        end, nil, true))
 
 		-- Remove/modify unwanted HTML elements to get a clean webpage.
-		htmlElement:select("div.on-top-nav"):remove() -- top navigation
-		htmlElement:select("div.chapter-list-wrapper"):remove() -- bottom navigation
+		--htmlElement:select("div.example"):remove()
 
 		return pageOfElem(htmlElement, true)
 	end,
@@ -54,35 +62,25 @@ return {
 	parseNovel = function(novelURL, loadChapters)
 		local doc = GETDocument(expandURL(novelURL))
 
-		local main = doc:selectFirst("main#primary")
-		local info_wrapper = main:selectFirst("div.info-wrapper")
-		local details = info_wrapper:selectFirst("div#top-info")
+		local main = doc:selectFirst("main > div > div")
+		local info_wrapper = main:selectFirst("div.col-12.col-lg-9 > div")
+		local details = info_wrapper:selectFirst("div.col-12.col-sm-9")
 
 		local info = NovelInfo {
-			title = details:selectFirst("h1"):text(),
-			imageURL = info_wrapper:selectFirst("img"):attr("src"),
-			description = info_wrapper:selectFirst("div#description p"):text(),
-			status = ({
-				Ongoing = NovelStatus.PUBLISHING,
-				Completed = NovelStatus.COMPLETED,
-			})[details:selectFirst("div.novel-status"):text()],
-			genres = map(details:select("div.genre-title"), text),
+			title = details:selectFirst("h3"):text(),
+			imageURL = expandURL(info_wrapper:selectFirst("img"):attr("src")),
+			description = info_wrapper:selectFirst("div#novel-desc"):attr("data-desc"),
+			genres = map(details:selectFirst("div.genre-container"):select("div.genre-el > a"), text),
+			authors = map(details:selectFirst("div.author-container"):select("div.author-el > a"), text),
 		}
-		local selector = details:selectFirst("div.author-wrapper")
-		if selector ~= nil then
-			info:setAuthors(map(selector:select("a"), text))
-		end
-		selector = details:selectFirst("div.alternative-name")
-		if selector ~= nil then
-			info:setAlternativeTitles(map(selector:select("h2"), text))
-		end
 
 		if loadChapters then
-			local chapter_list = main:selectFirst("div#chapter-list")
+			local chapter_list = main:selectFirst("div#chapter-list-content")
 			local curOrder = 0
+
 			info:setChapters(
 				AsList(
-					map(chapter_list:select("h3.chapter-title a"), function(v)
+					map(chapter_list:select("div > div > a"), function(v)
 						curOrder = curOrder + 1
 						return NovelChapter {
 							order = curOrder,
@@ -98,8 +96,43 @@ return {
 	end,
 
 	hasSearch = true,
-	isSearchIncrementing = true,
+	isSearchIncrementing = false,
 	search = function(data)
-		return parseTop(GETDocument(expandURL("/page/" .. data[PAGE] .. "/?s=" .. encode(data[QUERY]))))
+        -- Create JSON body for POST request.
+		local requestJson = json.encode({
+			clicked = false,
+			limit = "200", -- increased limit to limit to a single request
+			page = 0, -- therefore, only request first page
+			pageCount = 1,
+			value = data[QUERY],
+			sort = 4,
+			selected = {
+				genre = {},
+				status = {},
+				sort = {},
+				author = {}
+			},
+			results = {},
+			label = "searching ....",
+		})
+
+        local body = RequestBody(requestJson, MTYPE)
+        local post = RequestDocument(POST("https://octopii.co/api/advance-search", nil, body)):toString()
+
+        -- Cut HTML body from the actual JSON answer and decode it.
+        post = post:sub(33, -18)
+        local decodedJson = json.decode(post)
+
+        -- Get novels from the JSON.
+        local novels = {}
+        for _, n in ipairs(decodedJson.results) do
+            novels[#novels + 1] = Novel {
+                title = n.novel_name,
+                link = "/novel/" .. n.novel_slug,
+                imageURL = expandURL(n.image)
+            }
+        end
+
+		return novels
 	end,
 }
