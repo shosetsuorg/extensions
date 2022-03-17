f9dd17edf35fed165ca820192499a733d30758b9
doomsdayrs <doomsdayrs@gmail.com>
2022-03-16 23:46:19 -0400

    Patch RoyalRoad filter handling
    KYLEEEEEEEEEEEEEEEEEEEE
    
    Anyhows, This simply changes the else to an else if.

--- src/en/RoyalRoad.lua
+++ src/en/RoyalRoad.lua
@@ -182,9 +182,9 @@ local function createFilterString(data)
 	local function MultiTriQuery(strings, start, len)
 		for i=start+1,start+len do
 			if data[i] then
-				if data[i] == 2 then
+				if (data[i] == 2) then
 					tagsRemove[#tagsRemove+1] = strings[i]
-				else
+				elseif (data[i] == 1) then
 					tagsAdd[#tagsAdd+1] = strings[i]
 				end
 			end
