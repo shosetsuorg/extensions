6e472cc19bf25f5e33f5eccb02d2daa75a48f4b6
Rider21 <58046032+Rider21@users.noreply.github.com>
2022-09-28 19:00:41 +0300

    fix

--- src/ru/Ranobehub.lua
+++ src/ru/Ranobehub.lua
@@ -5,8 +5,7 @@ local baseURL = "https://ranobehub.org"
 local json = Require("dkjson")
 
 local ORDER_BY_FILTER = 3
-local ORDER_BY_TERMS = { "computed_rating", "last_chapter_at", "created_at", "name_rus", "views", "count_chapters",
-	"count_of_symbols" }
+local ORDER_BY_TERMS = { "computed_rating", "last_chapter_at", "created_at", "name_rus", "views", "count_chapters", "count_of_symbols" }
 
 local function shrinkURL(url)
 	return url:gsub(baseURL .. "/", "")
@@ -18,18 +17,28 @@ end
 
 local function getPassage(chapterURL)
 	local doc = GETDocument(expandURL(chapterURL))
-	local html = doc:selectFirst("div.text:nth-child(1)")
-	doc:select(".ads-desktop"):remove()
-	return pageOfElem(html)
+	local chap = doc:selectFirst("div.text:nth-child(1)")
+	chap:select(".ads-desktop"):remove()
+	chap:select("div.or:nth-child(1)"):remove()
+	chap:select(".title-wrapper"):remove()
+	chap:child(0):before("<h1>" .. doc:selectFirst("head > title"):text() .. "</h1>");
+
+	--[[
+	map(chap:select("img"), function(v)
+		v:setAttribute("src", baseURL .. "/api/media/" .. v:attr("data-media-id"))
+	end)
+	]]
+
+	return pageOfElem(chap, true)
 end
 
 local function parseNovel(novelURL, loadChapters)
 	local response = json.GET(baseURL .. "/api/" .. novelURL)
 
 	local novel = NovelInfo {
-		title = response.data.names.rus,
-		genres = map(response.data.tags.genres, function(v) return v.names.rus end),
-		tags = map(response.data.tags.events, function(v) return v.names.rus end),
+		title = response.data.names.rus or response.data.names.eng,
+		genres = map(response.data.tags.genres, function(v) return v.names.rus or v.names.eng end),
+		tags = map(response.data.tags.events, function(v) return v.names.rus or v.names.eng end),
 		imageURL = response.data.posters.medium,
 		description = Document(response.data.description):text(),
 		authors = { response.data.authors[1].name_eng },
@@ -47,9 +56,9 @@ local function parseNovel(novelURL, loadChapters)
 		for k1, volumes in ipairs(chapterJson.volumes) do
 			for k2, v2 in ipairs(chapterJson.volumes[k1].chapters) do
 				table.insert(chapterList, NovelChapter {
-					title = v2.name,
+					title = "Том " .. volumes.num .. ": " .. v2.name,
 					link = shrinkURL(v2.url),
-					release = tonumber(v2.changed_at) * 1000,
+					release = os.date("%Y-%m-%d %H:%M:%S", v2.changed_at),
 					order = chapterOrder
 				});
 				chapterOrder = chapterOrder + 1
@@ -60,8 +69,23 @@ local function parseNovel(novelURL, loadChapters)
 	return novel
 end
 
-local function search(filters)
-	return {}
+local function search(data)
+	local response = json.GET(expandURL("api/fulltext/global?query=" .. data[QUERY]))
+	local novels = {}
+
+	for k, v in pairs(response) do
+		if v.meta.key == "ranobe" then
+			novels = map(v.data, function(v2)
+				return Novel {
+					title = v2.names.rus or v2.names.eng,
+					link = "ranobe/" .. v2.id,
+					imageURL = v2.image:gsub("/small", "/medium")
+				}
+			end)
+		end
+	end
+
+	return novels
 end
 
 return {
@@ -69,11 +93,10 @@ return {
 	name = "Ranobehub",
 	baseURL = baseURL,
 
-	-- Optional values to change
-	imageURL = "https://ranobehub.org/favicon.png",
+	imageURL = "https://github.com/shosetsuorg/extensions/raw/dev/icons/Ranobehub.png",
+	chapterType = ChapterType.HTML,
 	hasSearch = true,
 
-	-- Must have at least one value
 	listings = {
 		Listing("Novel List", true, function(data)
 			local url = baseURL .. "/api/search?take=50&page=" .. data[PAGE]
@@ -88,7 +111,7 @@ return {
 			local d = json.GET(url)
 			return map(d.resource, function(v)
 				return Novel {
-					title = v.names.rus,
+					title = v.names.rus or v.names.eng,
 					link = "ranobe/" .. v.id,
 					imageURL = v.poster.medium
 				}
@@ -97,7 +120,7 @@ return {
 	},
 
 	searchFilters = {
-		DropdownFilter(ORDER_BY_FILTER, "Сортировка", {"Рейтинг", "Дате обновления", "Дате добавления", "Название", "Просмотры", "Количеству глав", "объем перевода" }),
+		DropdownFilter(ORDER_BY_FILTER, "Сортировка", { "Рейтинг", "Дате обновления", "Дате добавления", "Название", "Просмотры", "Количеству глав", "объем перевода" }),
 	},
 	getPassage = getPassage,
 	parseNovel = parseNovel,
