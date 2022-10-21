f8648b1e1c0b4df3fa1d4d9553f5660937aa7613
Alvian <xanvial@gmail.com>
2022-10-21 23:46:38 +0700

    Implement Asura Light Novel

---
+++ src/en/AsuraLightNovel.lua
@@ -0,0 +1,30 @@
+-- {"id":1783,"ver":"1.0.0","libVer":"1.0.0","author":"Xanvial","dep":["Madara>=2.4.0"]}
+
+return Require("Madara")("https://asuralightnovel.com",{
+	id = 1783,
+	name = "Asura Light Novel",
+	imageURL = "https://asuralightnovel.com/wp-content/uploads/2017/10/asura-light-novel-logo-4-e1627846850601.png",
+
+	searchHasOper = true,
+	ajaxUsesFormData = true,
+
+	genres = {
+		"Action",
+		"Adventure",
+		"Comedy",
+		"Fantasy",
+		"Harem",
+		"Isekai",
+		"Martial Arts",
+		"Mature",
+		"Mystery",
+		"Psychological",
+		"Romance",
+		"Sci-fi",
+		"Shounen",
+		"Supernatural",
+		"Tragedy",
+		"Xianxia",
+		"Xuanhuan"
+	}
+})
