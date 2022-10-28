90afe55170aa55354f1d15225b2e1a51ab06e3e0
doomsdayrs <doomsdayrs@gmail.com>
2022-10-28 00:46:07 -0400

    Add parseNovel function, search next

--- src/en/ReaperScans.lua
+++ src/en/ReaperScans.lua
@@ -10,25 +10,95 @@ local function expandURL(url)
 	return baseURL .. "/novels/" .. url
 end
 
-local function getPassage(chapterURL){
+local function getPassage(chapterURL)
 	local url = baseURL .. "/" .. chapterURL
 	local document = GETDocument(url)
 	local htmlElement = document:selectFirst("section.p-2")
 
 	return pageOfElem(htmlElement, true)
-}
+end
 
-local parseNovel(novelURL){
+local function parseNovel(novelURL)
 	local url = baseURL .. "/" .. novelURL
 	local document = GETDocument(url):selectFirst("article.post")
+	local novelInfo = NovelInfo()
 	
-}
+	local headerNChapters = document:selectFirst("div.p-2.space-y-4")
+	
+	-- Header
+	local headerElement = headerNChapters:selectFirst("div.mx-auto")
+
+	local titleElement = headerNChapters:selectFirst("h1")
+	novelInfo:setTitle(titleElement:text())
+
+	local imageElement = headerElement:selectFirst("img")
+	novelInfo:setImageURL(imageElement:attr("src"))
+	
+	-- About	
+	local aboutElement = document:selectFirst("div.lg\:col-span-1")
+
+	local descriptionElement = aboutElement:selectFirst("p")
+	novelInfo:setDescription(descriptionElement:text():gsub("<br>","\n"))
+		
+	local otherElements = document:selectFirst("dl.mt-2"):select("dd")
+	
+	local languageElement = otherElements[0]
+	novelInfo:setLanguage(languageElement:text())
+
+	local statusElement = otherElements[1]
+	local status = statusElement:text()
+	novelInfo:setStatus(NovelStatus(status == "Completed" and 1 or status == "Ongoing" and 0 or 3))
+
+	-- Chapters
+	local chaptersBoxElement = headerNChapters:selectFirst("pb-4"):selectFirst("ul")
+
+	local pages = 0
+	pages = chaptersBoxElement:selectFirst("div.releative.z-0"):select("button"):size()
+	
+	-- After the following loop, this array will contain a list of chapters reversed.
+	local chapterElements = {}
+	chapterElements.concat(chaptersBoxElement:select("li"))
+	
+	for page = 0, pages, 1
+	do
+		document = GETDocument(url .. "?page=" .. page):selectFirst("article.post")
+		headerNChapters = document:selectFirst("div.p-2.space-y-4")
+		chaptersBoxElement = headerNChapters:selectFirst("pb-4"):selectFirst("ul")
+		chapterElements.concat(chaptersBoxElement:select("li"))
+	end
+
+	local count = 0
+	local chapters = map(
+		chapterElements,
+		function(chapter)
+			local c = NovelChapter()
+			c:setTitle(chapter:selectFirst("p"):text())
+			c:setLink(chapter:selectFirst("a"):attr("href"))
+			
+			-- count the chapters
+			count = count + 1
+			return c
+		end
+	)
+	chapters = map(
+		chapters,
+		function(chapter)
+			chapter:setOrder(count)
+			count = count - 1
+			return chapter
+		end
+	)
+	Reverse(chapters)
+	novelInfo:setChapters(chapters)	
+	
+	return novelInfo
+end
 
 return {
 	id = 4223,
-	name = "MTLNovel",
+	name = "Reaper Scans",
 	baseURL = baseURL,
-	imageURl imageURL = "https://github.com/shosetsuorg/extensions/raw/dev/icons/ReaperScans.png",
+	imageURL = "https://github.com/shosetsuorg/extensions/raw/dev/icons/ReaperScans.png",
 	hasSearch = true,
 	chapterType = ChapterType.HTML,
 
@@ -67,5 +137,6 @@ return {
 	},
 	getPassage = getPassage,
 	parseNovel = parseNovel,
-	search = search
+	hasSearch = false
+	--search = search
 }
