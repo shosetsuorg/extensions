d4e2ccfa35c9192d1017da99027749ecd39e8772
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2022-12-04 00:42:32 +0530

    update novelcake.lua

--- src/en/novelcake.lua
+++ src/en/novelcake.lua
@@ -7,7 +7,7 @@ return Require("Madara")("https://novelcake.com", {
   shrinkURLNovel = "series",
   novelListingURLPath = "series",
   latestNovelSel = "div.col-6.col-md-3",
-ajaxUsesFormData = false,
+  ajaxUsesFormData = true,
   chaptersScriptLoaded = true,
 
   genres = {
