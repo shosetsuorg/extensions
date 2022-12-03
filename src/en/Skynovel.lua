bdd510fd34968f3af1ccc69b0650053004a93bdc
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2022-12-03 21:26:59 +0530

    Add files via upload

---
+++ src/en/Skynovel.lua
@@ -0,0 +1,49 @@
+-- {"id":2,"ver":"3.1.0","libVer":"1.0.0","author":"Doomsdayrs","dep":["Madara>=2.2.0"]}
+
+return Require("Madara")("https://skynovel.org", {
+  id = 75,
+  name = "Skynovel",
+  imageURL = "",
+  path = {
+    "novels",
+    "manga",
+    "novel",
+    "manga",
+    "chapter",
+    "manga"
+  }
+
+  genres = {
+    "Action",
+    "Adventure",
+    "Comedy",
+    "Drama",
+    "Ecchi",
+    "Fantasy",
+    "Gender Bender",
+    "Harem",
+    "Historical",
+    "Horror",
+    "Josei",
+    "Martial Arts",
+    "Mature",
+    "Mecha",
+    "Mystery",
+    "Psychological",
+    "Romance",
+    "School Life",
+    "Sci-fi",
+    "Seinen",
+    "Shoujo",
+    "Shounen",
+    "Slice of Life",
+    "Smut",
+    "Sports",
+    "Supernatural",
+    "Tragedy",
+    "Wuxia",
+    "Xianxia",
+    "Xuanhuan",
+    "Yaoi"
+  }
+})
