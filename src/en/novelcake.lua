d9ee785ac45b850b8ea720ecf5156a288c0ced78
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2022-12-03 23:10:39 +0530

    update novelcake.lua

--- src/en/novelcake.lua
+++ src/en/novelcake.lua
@@ -5,7 +5,9 @@ return Require("Madara")("https://novelcake.com", {
   name = "novelcake",
   imageURL = "https://github.com/shosetsuorg/extensions/raw/dev/icons/BoxNovel.png",
   novelListingURLPath = "series",
-
+  shrinkURLNovel = "novels",
+  ajaxUsesFormData = true,
+  
   genres = {
     "Action",
     "Adventure",
