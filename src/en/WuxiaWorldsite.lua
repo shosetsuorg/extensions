e894c65ae9bb166528c3a1956cad1ac78e8c2e53
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2022-12-04 00:59:17 +0530

    Add files via upload

---
+++ src/en/WuxiaWorldsite.lua
@@ -0,0 +1,45 @@
+-- {"id":75,"ver":"1.0.5","libVer":"1.0.0","author":["Doomsdayrs", "AbhiTheModder"],"dep":["Madara>=2.2.0"]}
+
+return Require("Madara")("https:// WuxiaWorld.site", {
+  id = 75,
+  name = "WuxiaWorldsite",
+  imageURL = "https://novelcake.com/wp-content/uploads/2017/10/NOVELCAKE_2.png",
+  shrinkURLNovel = "novel",
+  novelListingURLPath = "novels-list",
+  latestNovelSel = "div.col-6.col-md-3",
+  chaptersScriptLoaded = true,
+
+  genres = {
+    "Action",
+    "Adventure",
+    "Comedy",
+    "Drama",
+    "Ecchi",
+    "Fantasy",
+    "Gender Bender",
+    "Harem",
+    "Historical",
+    "Horror",
+    "Josei",
+    "Martial Arts",
+    "Mature",
+    "Mecha",
+    "Mystery",
+    "Psychological",
+    "Romance",
+    "School Life",
+    "Sci-fi",
+    "Seinen",
+    "Shoujo",
+    "Shounen",
+    "Slice of Life",
+    "Smut",
+    "Sports",
+    "Supernatural",
+    "Tragedy",
+    "Wuxia",
+    "Xianxia",
+    "Xuanhuan",
+    "Yaoi"
+  }
+})
