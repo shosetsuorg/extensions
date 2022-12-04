6c8231f1ba7a0450ff138a3a256beb41ffcfb695
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2022-12-04 14:51:11 +0530

    update Novel-gate.lua

--- src/en/Novel-gate.lua
+++ src/en/Novel-gate.lua
@@ -1,11 +1,11 @@
 -- {"id":77,"ver":"1.0.1","libVer":"1.0.0","author":["Doomsdayrs", "AbhiTheModder"],"dep":["Madara>=2.2.0"]}
 
-return Require("Madara")("https://novel-gate.com", {
+return Require("Madara")("https://sky-mtl.com", {
   id = 77,
-  name = "Novel-gate",
-  imageURL = "",
+  name = "Sky-mtl",
+  imageURL = "https://sky-mtl.com/wp-content/uploads/2017/10/10.png",
   shrinkURLNovel = "novel",
-  novelListingURLPath = "novels",
+  novelListingURLPath = "novel",
   latestNovelSel = "div.col-6.col-md-3",
   chaptersScriptLoaded = true,
   hasSearch = true,
