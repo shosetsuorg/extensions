ad8abe77642363ae6c1a4184d2e95cb83c4b3830
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2022-12-04 10:59:01 +0530

    Add files via upload

---
+++ src/en/Novel-gate.lua
@@ -0,0 +1,44 @@
+-- {"id":77,"ver":"1.0.1","libVer":"1.0.0","author":["Doomsdayrs", "AbhiTheModder"],"dep":["Madara>=2.2.0"]}
+
+return Require("Madara")("https://novel-gate.com", {
+  id = 77,
+  name = "Novel-gate",
+  imageURL = "",
+  novelListingURLPath = "novels",
+  latestNovelSel = "div.col-6.col-md-3",
+  chaptersScriptLoaded = true,
+
+  genres = {
+    "Action",
+    "Adventure",
+    "Comedy",
+    "Drama",
+    "Ecchi",
+    "Fantasy",
+    "Gender Bender",
+    "Harem",
+    "Historical",
+    "Horror",
+    "Josei",
+    "Martial Arts",
+    "Mature",
+    "Mecha",
+    "Mystery",
+    "Psychological",
+    "Romance",
+    "School Life",
+    "Sci-fi",
+    "Seinen",
+    "Shoujo",
+    "Shounen",
+    "Slice of Life",
+    "Smut",
+    "Sports",
+    "Supernatural",
+    "Tragedy",
+    "Wuxia",
+    "Xianxia",
+    "Xuanhuan",
+    "Yaoi"
+  }
+})
