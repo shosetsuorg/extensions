f5d6889a29af2f5a815ce8062f4fbba83f2dfc34
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2022-12-04 14:52:47 +0530

    Rename Novel-gate.lua to Sky-mtl.lua

---
+++ src/en/Sky-mtl.lua
@@ -0,0 +1,47 @@
+-- {"id":77,"ver":"1.0.1","libVer":"1.0.0","author":["Doomsdayrs", "AbhiTheModder"],"dep":["Madara>=2.2.0"]}
+
+return Require("Madara")("https://sky-mtl.com", {
+  id = 77,
+  name = "Sky-mtl",
+  imageURL = "https://sky-mtl.com/wp-content/uploads/2017/10/10.png",
+  shrinkURLNovel = "novel",
+  novelListingURLPath = "novel",
+  latestNovelSel = "div.col-6.col-md-3",
+  chaptersScriptLoaded = true,
+  hasSearch = true,
+  searchHasOper = true,
+
+  genres = {
+    "Action",
+    "Adventure",
+    "Comedy",
+    "Drama",
+    "Ecchi",
+    "Fantasy",
+    "Gender Bender",
+    "Harem",
+    "Historical",
+    "Horror",
+    "Josei",
+    "Martial Arts",
+    "Mature",
+    "Mecha",
+    "Mystery",
+    "Psychological",
+    "Romance",
+    "School Life",
+    "Sci-fi",
+    "Seinen",
+    "Shoujo",
+    "Shounen",
+    "Slice of Life",
+    "Smut",
+    "Sports",
+    "Supernatural",
+    "Tragedy",
+    "Wuxia",
+    "Xianxia",
+    "Xuanhuan",
+    "Yaoi"
+  }
+})
